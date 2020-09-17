2f4d55409697ebe08ae06859ec91ae2011be5197
Brian Norton <brian.nort@gmail.com>
2020-09-17 08:20:33 -0700

    Move app.js to it's rightful place.

--- docs/app.js
+++ docs/app.js
@@ -1,13 +1,13 @@
 !function() {
-  document.readyState === 'complete' ? start() :
-    d.addEventListener('readystatechange', function() { d.readyState === 'complete' && start(); });
-
   window.app = {
     $http: http(),
     info: { tzHours: -(new Date().getTimezoneOffset()/60) },
     keys: { segment: 'WmlbvdtG9hJU7p75UIGtZcZbnpg8ibkC' },
     data: { account: { }, user: { } },
   };
+
+  document.readyState === 'complete' ? start() :
+    d.addEventListener('readystatechange', function() { d.readyState === 'complete' && start(); });
 
   function start() {
     var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(!analytics.invoked){analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
@@ -23,6 +23,7 @@
   }
 
   function identify() {
+    console.log('identify', app.data.user.id)
     if(!app.data.user.id) {
       return;
     }
